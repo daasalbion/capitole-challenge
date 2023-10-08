e81d6b5708ffa11d5e5b91cb5c4c763ef34c78ae
Derlis Arguello <derlisarguello01@gmail.com>
2023-10-08 18:22:10 -0300

    first commit

---
+++ src/main/java/py/com/daas/capitolechallenge/CapitoleChallengeApplication.java
@@ -0,0 +1,13 @@
+package py.com.daas.capitolechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitoleChallengeApplication.class, args);
+    }
+
+}
